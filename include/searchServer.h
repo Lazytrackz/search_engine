7089ea1f7ee0003f0be8dd0dce8f35446754da59
WZZ85 <lazytrackz@gmail.com>
2022-05-07 20:09:04 +0300

    Edit config.json and answers.json. Add new Cmake

--- include/searchServer.h
+++ include/searchServer.h
@@ -27,7 +27,6 @@ class SearchServer {
 public:
 
     SearchServer(InvertedIndex& idx) : _index(idx){ };
-
     std::vector<std::vector<RelativeIndex>> search(const std::vector<std::string>& queries_input);
 
 private:
